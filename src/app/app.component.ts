c2f9d6d02b8d6d858e906de3e34a25f3ced31179
olcay <olcayusta02@gmail.com>
2020-08-13 02:26:51 +0300

    02:26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, InjectionToken, Injector, OnInit, ViewChild } from '@angular/core';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal, PortalInjector } from '@angular/cdk/portal';
+import { MiniPlayerComponent } from './mini-player/mini-player.component';
+import { DataService } from './data.service';
+
+export const CONTAINER_DATA = new InjectionToken<{}>('CONTAINER_DATA');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'player-to-mini-player';
+export class AppComponent implements OnInit {
+  title = 'video-app';
+  isOpened = false;
+
+  // copyVideo;
+
+  // myVideo;
+
+  state = 'normal';
+
+  positions!: any;
+
+  @ViewChild('videoElement', {static: true, read: ElementRef}) videoEl!: ElementRef<HTMLVideoElement>;
+  @ViewChild('normalPlayer', {static: true, read: ElementRef}) normalPlayer!: ElementRef;
+  @ViewChild('miniPlayer', {static: true, read: ElementRef}) miniPlayer!: ElementRef;
+  @ViewChild('overlayRef', {static: true}) overlayRef!: OverlayRef;
+
+  constructor(public overlay: Overlay, private dataService: DataService, private injector: Injector) {
+    this.positions = this.overlay.position().global().bottom('24px').right('24px');
+  }
+
+  ngOnInit(): void {
+    /*    this.myVideo = document.createElement('video');
+        this.myVideo.src = 'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4';
+        setTimeout(() => {
+          this.myVideo.play();
+        });*/
+    console.log(this.overlayRef);
+  }
+
+  onTimeUpdate(): void {
+    this.dataService.setTime(this.videoEl.nativeElement.currentTime);
+  }
+
+  createInjector(dataToPass: any): PortalInjector {
+    const injectorTokens = new WeakMap();
+    injectorTokens.set(CONTAINER_DATA, dataToPass);
+    return new PortalInjector(this.injector, injectorTokens);
+  }
+
+  setNormalPlayer(): void {
+    this.isOpened = true;
+    this.state = 'normal';
+    this.normalPlayer.nativeElement.appendChild(this.videoEl.nativeElement);
+  }
+
+  // Method 1
+  addPlayerToHTMLDiv(): void {
+    this.miniPlayer.nativeElement.appendChild(this.videoEl.nativeElement);
+  }
+
+  setMiniPlayer(): void {
+    this.state = 'mini';
+
+/*    const overlayRef = this.overlay.create({
+      width: '360px',
+      positionStrategy: this.overlay.position().global().top('24px').right('24px')
+    });
+    const miniPlayerPortal = new ComponentPortal(MiniPlayerComponent, null, this.createInjector({
+      givenName: 'Kate',
+      familyName: 'Upton'
+    }));
+    overlayRef.attach(miniPlayerPortal);*/
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MiniPlayerComponent } from './mini-player/mini-player.component';
+import { FullscreenOverlayContainer, OverlayContainer, OverlayModule } from '@angular/cdk/overlay';
+
+export const MY_TOKEN = new InjectionToken<string>('MyToken');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiniPlayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OverlayModule
   ],
-  providers: [],
+  providers: [
+    {provide: MY_TOKEN, useValue: 'Hello World'},
+    // {provide: OverlayContainer, useClass: FullscreenOverlayContainer}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
